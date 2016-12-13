12d144cb3a5e3b30dd37b6047172305374d3d40e
Raimon Esteve <raimon@nan-tic.com>
2016-12-13 10:20:58 +0100

    Change readonly when state is done

--- shipment.py
+++ shipment.py
@@ -6,20 +6,17 @@ from trytond.pool import PoolMeta
 from trytond.pyson import Eval
 
 __all__ = ['ShipmentOut']
-__metaclass__ = PoolMeta
 
 
 class ShipmentOut:
-    "Customer Shipment"
+    __metaclass__ = PoolMeta
     __name__ = 'stock.shipment.out'
     carrier_tracking_ref = fields.Char("Carrier Tracking Ref", states={
-            'readonly': ~Eval('state').in_(['draft', 'waiting', 'assigned',
-                    'packed']),
-            }, depends=['state'])
+        'readonly': Eval('state') == 'done',
+        }, depends=['state'])
     number_packages = fields.Integer('Number of Packages', states={
-            'readonly': ~Eval('state').in_(['draft', 'waiting', 'assigned',
-                    'packed']),
-            }, depends=['state'])
+        'readonly': Eval('state') == 'done',
+        }, depends=['state'])
 
     @staticmethod
     def default_number_packages():
