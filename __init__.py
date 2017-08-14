980d0ba2589a978c48dac22d25c2e20b97c95c7d
?ngel ?lvarez <angel@nan-tic.com>
2017-08-14 16:30:31 +0200

    fix view, pep8 and flake

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-#This file is part stock_delivery module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part stock_delivery module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.pool import Pool
-from .shipment import *
+from . import shipment
 
 
 def register():
     Pool.register(
-        ShipmentOut,
+        shipment.ShipmentOut,
         module='stock_delivery', type_='model')
